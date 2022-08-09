adb5130bd9665870ff5daee05b599cd2c0af4643
Joao Antonio <joaopsilva.dev@gmail.com>
2022-08-09 16:01:31 -0300

    feat: :construction: primeiro requisito completo
    criando a classe Race

---
+++ src/Races/Race.ts
@@ -0,0 +1,25 @@
+export default abstract class Race {
+  private readonly _name: string;
+  private readonly _dexterity: number;
+  abstract get maxLifePoints(): number;
+  
+  constructor(
+    name: string,
+    dexterity: number,
+  ) {
+    this._name = name;
+    this._dexterity = dexterity;
+  }
+
+  get name(): string {
+    return this._name;
+  }
+
+  get dexterity(): number {
+    return this._dexterity;
+  }
+
+  static createdRacesInstances(): number {
+    throw new Error('Not implemented');
+  }
+}
